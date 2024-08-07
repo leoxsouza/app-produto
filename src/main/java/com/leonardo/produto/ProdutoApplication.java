58cdb74cc3301aa650a175c7f5d57d91d8d11d40
Leonardo Oliveira <leoxsouza286@gmail.com>
2024-08-07 15:00:37 -0300

    feat: setup inicial

---
+++ src/main/java/com/leonardo/produto/ProdutoApplication.java
@@ -0,0 +1,13 @@
+package com.leonardo.produto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoApplication.class, args);
+	}
+
+}
